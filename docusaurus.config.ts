42714f7712002c2e7bfb85c0d59f36d3e2b35f83
daixiheguu <daixihegu@outlook.com>
2026-08-28 11:43:31 +0800

    chore(config): migrate broken markdown link hook (#362)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,11 @@ const config: Config = {
 
   trailingSlash: false,
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   i18n: {
     defaultLocale: 'en',
